ce782e848fa16c96c013794227dff57d4708e876
Niko Lepka <slench102+git@gmail.com>
2013-11-22 12:47:43 +0100

    Fixed the hashing algorithms
    they work now

--- Utility/Methods/TextHasher.cs
+++ Utility/Methods/TextHasher.cs
@@ -8,11 +8,23 @@ using System.Threading.Tasks;
 namespace Utility.Methods
 {
     public enum HashType { MD5, SHA512, SHA256, SHA384, SHA1 }
-    public enum HashSubType {Normal, Cng, Managed, CryptoServiceProvider}
+    public enum HashSubType { Normal, Cng, Managed, CryptoServiceProvider }
 
     public static class TextHasher
     {
-        public static string Hash(this string input, HashType hash, HashSubType subType = HashSubType.Normal)
+        private const string BASE = "System.Security.Cryptography.";
+
+        public const string
+            BASE_MD5 = BASE + "MD5",
+            BASE_SHA1 = BASE + "SHA1",
+            BASE_SHA256 = BASE + "SHA256",
+            BASE_SHA384 = BASE + "SHA384",
+            BASE_SHA512 = BASE + "SHA512",
+            TYPE_CNG = "Cng",
+            TYPE_MANAGED = "Managed",
+            TYPE_CSP = "CryptoServiceProvider";
+
+        public static string GenerateHash(this string input, HashType hash, HashSubType subType = HashSubType.Normal)
         {
             Func<HashAlgorithm, string> hashFunction = alg => HashingHelper(input, alg);
 
@@ -45,15 +57,15 @@ namespace Utility.Methods
             switch (hash)
             {
                 case HashType.MD5:
-                    return MD5.Create();
+                    return MD5.Create(BASE_MD5);
                 case HashType.SHA1:
-                    return SHA1.Create();
+                    return SHA1.Create(BASE_SHA1);
                 case HashType.SHA256:
-                    return SHA256.Create();
+                    return SHA256.Create(BASE_SHA256);
                 case HashType.SHA384:
-                    return SHA384.Create();
+                    return SHA384.Create(BASE_SHA384);
                 case HashType.SHA512:
-                    return SHA512.Create();
+                    return SHA512.Create(BASE_SHA512);
                 default: return null; // unreachable
             }
         }
@@ -63,15 +75,15 @@ namespace Utility.Methods
             switch (hash)
             {
                 case HashType.MD5:
-                    return MD5Cng.Create();
+                    return MD5.Create(BASE_MD5 + TYPE_CNG);
                 case HashType.SHA1:
-                    return SHA1Cng.Create();
+                    return SHA1.Create(BASE_SHA1 + TYPE_CNG);
                 case HashType.SHA256:
-                    return SHA256Cng.Create();
+                    return SHA256.Create(BASE_SHA256 + TYPE_CNG);
                 case HashType.SHA384:
-                    return SHA384Cng.Create();
+                    return SHA384.Create(BASE_SHA384 + TYPE_CNG);
                 case HashType.SHA512:
-                    return SHA512Cng.Create();
+                    return SHA512.Create(BASE_SHA512 + TYPE_CNG);
                 default: return null; // unreachable
             }
         }
@@ -80,14 +92,15 @@ namespace Utility.Methods
         {
             switch (hash)
             {
+                // MD5 not available
                 case HashType.SHA1:
-                    return SHA1Managed.Create();
+                    return SHA1.Create(BASE_SHA1 + TYPE_MANAGED);
                 case HashType.SHA256:
-                    return SHA256Managed.Create();
+                    return SHA256.Create(BASE_SHA256 + TYPE_MANAGED);
                 case HashType.SHA384:
-                    return SHA384Managed.Create();
+                    return SHA384.Create(BASE_SHA384 + TYPE_MANAGED);
                 case HashType.SHA512:
-                    return SHA512Managed.Create();
+                    return SHA512.Create(BASE_SHA512 + TYPE_MANAGED);
                 default: return null; // unreachable
             }
         }
@@ -97,15 +110,15 @@ namespace Utility.Methods
             switch (hash)
             {
                 case HashType.MD5:
-                    return MD5CryptoServiceProvider.Create();
+                    return MD5.Create(BASE_MD5 + TYPE_CSP);
                 case HashType.SHA1:
-                    return SHA1CryptoServiceProvider.Create();
+                    return SHA1.Create(BASE_SHA1 + TYPE_CSP);
                 case HashType.SHA256:
-                    return SHA256CryptoServiceProvider.Create();
+                    return SHA256.Create(BASE_SHA256 + TYPE_CSP);
                 case HashType.SHA384:
-                    return SHA384CryptoServiceProvider.Create();
+                    return SHA384.Create(BASE_SHA384 + TYPE_CSP);
                 case HashType.SHA512:
-                    return SHA512CryptoServiceProvider.Create();
+                    return SHA512.Create(BASE_SHA512 + TYPE_CSP);
                 default: return null; // unreachable
             }
         }
